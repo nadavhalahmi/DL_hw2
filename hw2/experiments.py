805c6e6f48ec994c817a833370cf1f381970effe
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-05-07 00:30:47 +0300

    part3 slow

--- hw2/experiments.py
+++ hw2/experiments.py
@@ -64,7 +64,16 @@ def run_experiment(run_name, out_dir='./results', seed=None, device=None,
     #   for you automatically.
     fit_res = None
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
+    dl_train = DataLoader(torch.utils.data.random_split(ds_train, [20000,len(ds_train)-20000])[0], batch_size=bs_train) #TODO: CHANGE THIS WIRED THING
+    dl_test = DataLoader(torch.utils.data.random_split(ds_test, [6000,len(ds_test)-6000])[0], batch_size=bs_test) #TODO: CHANGE THIS WIRED THING
+    total_filters = []
+    for filter in filters_per_layer:
+        total_filters += [filter]*layers_per_block
+    model = model_cls(ds_train[0][0].shape, len(ds_train.classes),total_filters, pool_every, hidden_dims)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    trainer = training.TorchTrainer(loss_fn=loss_fn, model=model, optimizer=optimizer)
+    fit_res = trainer.fit(dl_train, dl_test, num_epochs=epochs, checkpoints=checkpoints, early_stopping=early_stopping, **kw)
     # ========================
 
     save_experiment(run_name, out_dir, cfg, fit_res)
